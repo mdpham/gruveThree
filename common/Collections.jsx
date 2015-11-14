f4989535b1f208a61cf3110467ea53966db4794c
Martin Pham <mdpham@uwaterloo.ca>
2015-11-14 19:34:10 +0000

    permissions

--- common/Collections.jsx
+++ common/Collections.jsx
@@ -1,15 +1,57 @@
 //Collection of Soundcloud user IDs to update SCUserCollection with on sign in
 InfluencesCollection = new Mongo.Collection("influences");
-
+InfluencesCollection.allow({
+	insert() {
+		return true;
+	},
+	update() {
+		return true;
+	},
+	remove() {
+		return true;
+	}
+})
 //Collection of Soundcloud users
 SCUsersCollection = new Mongo.Collection("scUsers");
-
+SCUsersCollection.allow({
+	insert() {
+		return true;
+	},
+	update() {
+		return true;
+	},
+	remove() {
+		return true;
+	}
+})
 //Collection of Soundcloud users and their favorites (hearts on soundcloud)
 FavoritesCollection = new Mongo.Collection("favorites");
+FavoritesCollection.allow({
+	insert() {
+		return true;
+	},
+	update() {
+		return true;
+	},
+	remove() {
+		return true;
+	}
+})
 
 //Collection of user liked tracks
 //Holds track objects with ids in the profile object in user objects
 LikesCollection = new Mongo.Collection("likes");
+LikesCollection.allow({
+	insert() {
+		return true;
+	},
+	update() {
+		return true;
+	},
+	remove() {
+		return true;
+	}
+})
 
 
 
